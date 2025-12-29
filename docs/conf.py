3dd22634b4fb6575be7e163a2673f23fecdaff27
Vadim Marusin <vadim@estateblock.com>
2025-12-28 17:33:24 -0800

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,10 @@
+project = "Estateblock Canadian Real Estate Dataset"
+author = "Estateblock"
+release = "1.0"
+
+extensions = []
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+html_theme = "sphinx_rtd_theme"
